62fd014731929f1db6690ff7c1639a6a1841438c
Kevin Faaborg <zootella@zootella.com>
2025-08-03 16:53:28 -0600

    before template listeners

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,62 +1,51 @@
 <script setup>//./components/Table.vue - mvp lighttable for full screen
 
-import {getCurrentWindow} from '@tauri-apps/api/window'
-
 const imageSize = 900
 const tableSize = 3000
 
+import {getCurrentWindow} from '@tauri-apps/api/window'
 import {ref, onMounted, onBeforeUnmount} from 'vue'
 
 const frameRef = ref(null)//frame around boundaries of this component, likely the whole window or whole screen
 const tableRef = ref(null)//background yellow on black grid the user will be able to drag to pan around
 const imageRef = ref(null)//on top of that, centered, rendered to device pixels, the img tag that holds the image
 
-let tablePosition = {x: 0, y: 0}
-let dragStart = {x: 0, y: 0}
-let draggingPointer = null
-let draggingButton = -1//which button is dragging, 0 primary or 2 secondary
+let tablePosition = {x: 0, y: 0}//where the top left corner of the table is in CSS page coordinates
+let drag//an object of positions and ids during a left or right click drag
 
 onMounted(() => {
 
-	window.addEventListener('keydown', myKey)
-	frameRef.value.addEventListener('wheel', myWheel, {passive: false})
+	window.addEventListener('keydown', onKey)
+	frameRef.value.addEventListener('wheel', onWheel, {passive: false})
 
 	tableRef.value.style.transformOrigin = '0 0'
 	tableRef.value.style.transform = `translate(${tablePosition.x}px, ${tablePosition.y}px)`
-	tableRef.value.addEventListener('pointerdown', myPointerDown)
-	tableRef.value.addEventListener('dblclick', myDoubleClick)
-	tableRef.value.addEventListener('contextmenu', myContextMenu)
+	tableRef.value.addEventListener('pointerdown', onPointerDown)
+	tableRef.value.addEventListener('dblclick', onDoubleClick)
+
+	tableRef.value.addEventListener('contextmenu', e => e.preventDefault())//don't show a context menu; you think tauri only shows one in development mode; chat says this won't mess things up
 })
 onBeforeUnmount(() => {
 
-	window.removeEventListener('keydown', myKey)
-	frameRef.value.removeEventListener('wheel', myWheel)
-
-	tableRef.value.removeEventListener('pointermove', myPointerMove)//remove any remaining drag listeners
-	tableRef.value.removeEventListener('pointerup', myPointerUp)
-	tableRef.value.removeEventListener('pointerdown', myPointerDown)
-	tableRef.value.removeEventListener('dblclick', myDoubleClick)
-	tableRef.value.removeEventListener('contextmenu', myContextMenu)
-	if (draggingPointer) {
-		tableRef.value.releasePointerCapture(draggingPointer)
-		draggingPointer = null
+	window.removeEventListener('keydown', onKey)
+	frameRef.value.removeEventListener('wheel', onWheel)
+	['pointermove', 'pointerup', 'pointerdown', 'dblclick', 'contextmenu', 'pointercancel', 'lostpointercapture'].forEach(e => tableRef.value.removeEventListener(e, listenersMap[e]))
+	if (drag?.pointer) {
+		tableRef.value.releasePointerCapture(drag.pointer)
+		drag.pointer = null
 	}
 })
 
-function myContextMenu(e) {
-	e.preventDefault()//don't show a context menu; you think tauri only shows one in development mode; chat says this won't mess things up
-}
-
-async function myKey(e) {
+async function onKey(e) {
 	if (e.target.tagName == 'INPUT' || e.target.tagName == 'TEXTAREA' || e.target.isContentEditable) return//ignore keystrokes into a form field
 
 	if (e.key == 'f') {
 		console.log('my key F')
-		await myFullscreen(true)
+		await fullscreen(true)
 
 	} else if (e.key == 'q') {
 		console.log('my key Q')
-		await myFullscreen(false)
+		await fullscreen(false)
 
 	} else if ((e.ctrlKey || e.metaKey) && e.key == 's') {
 		e.preventDefault()//tell the browser not to show the file save dialog box
@@ -64,7 +53,7 @@ async function myKey(e) {
 
 	} else if (e.key == 'Escape') {
 		console.log('my key Escape')
-		await myFullscreen(false)//macos will also exit fullscreen, but this call doesn't mess anything up with that
+		await fullscreen(false)//macos will also exit fullscreen, but this call doesn't mess anything up with that
 
 	} else if (e.key == 'ArrowLeft')  { console.log('my key ArrowLeft')  }
 	else if (e.key == 'ArrowRight') { console.log('my key ArrowRight') }
@@ -73,7 +62,7 @@ async function myKey(e) {
 	else if (e.key == 'PageUp')     { console.log('my key PageUp')     }
 	else if (e.key == 'PageDown')   { console.log('my key PageDown')   }
 }
-async function myFullscreen(set) {
+async function fullscreen(set) {
 	let w = getCurrentWindow()
 	let current = await w.isFullscreen()
 	if (set != current) {
@@ -81,7 +70,8 @@ async function myFullscreen(set) {
 		w.setFullscreen(set)
 	}
 }
-function myWheel(e) {
+
+function onWheel(e) {
 	e.preventDefault()//tell the browser not to scroll
 
 	if (e.deltaY < 0) {
@@ -90,37 +80,43 @@ function myWheel(e) {
 		console.log('my wheel forward')
 	}
 }//ttd august, see how this flips out on a touchpad, though
-function myDoubleClick(e) {
+
+function onDoubleClick(e) {
 	console.log('double click event')
 }
-function myPointerDown(e) {
+
+function onPointerDown(e) {
 
 	if (e.button == 0 && e.detail == 2 && e.buttons == 1) {//primary button 0, 2nd quick click, first bit value 1 only button down right now
 		console.log('pointer down: double click')
 	} else if (e.button == 2 && e.detail == 2 && e.buttons == 2) {//secondary button 2, 2nd quick click, second bit value 2 only button down right now
 		console.log('pointer down: right double click')
 	} else {
-		myStartDrag(e)
+		dragStart(e)
 	}
 }
-function myStartDrag(e) {
-	draggingButton = e.button
-	draggingPointer = e.pointerId
-
-	dragStart.x = e.clientX//remember where the drag started
-	dragStart.y = e.clientY
+function dragStart(e) {
+	drag = {
+		start: {x: e.clientX, y: e.clientY},//record where this drag started
+		pointer: e.pointerId,
+		button: e.button,//0 primary or 2 secondary mouse button
+	}
 
 	tableRef.value.setPointerCapture(e.pointerId)//watch the mouse
-	tableRef.value.addEventListener('pointermove', myPointerMove)
-	tableRef.value.addEventListener('pointerup', myPointerUp)
+	tableRef.value.addEventListener('pointermove',        onMove)
+	tableRef.value.addEventListener('pointerup',          onUp)
+	tableRef.value.addEventListener('pointercancel',      onUp)
+	tableRef.value.addEventListener('lostpointercapture', onUp)
 }
-function myPointerMove(e) {
+function onMove(e) {
+	if (!drag) return
+
 	const segment = {//how far did the mouse move in this segment of the drag?
-		x: e.clientX - dragStart.x,
-		y: e.clientY - dragStart.y,
+		x: e.clientX - drag.start.x,
+		y: e.clientY - drag.start.y,
 	}
-	dragStart.x = e.clientX//get ready for the next segment if the drag continues
-	dragStart.y = e.clientY
+	drag.start.x = e.clientX//get ready for the next segment if the drag continues
+	drag.start.y = e.clientY
 
 	const candidate = {//what would our new table position be?
 		x: tablePosition.x + segment.x,
@@ -138,12 +134,16 @@ function myPointerMove(e) {
 
 	tableRef.value.style.transform = `translate(${tablePosition.x}px, ${tablePosition.y}px)`//push to the GPU
 }
-function myPointerUp(e) {
+function onUp(e) {
+	if (!drag) return
 
-	tableRef.value.removeEventListener('pointermove', myPointerMove)
-	tableRef.value.removeEventListener('pointerup',   myPointerUp)
-	tableRef.value.releasePointerCapture(e.pointerId)
-	draggingPointer = null
+	tableRef.value.removeEventListener('pointermove',        onMove)
+	tableRef.value.removeEventListener('pointerup',          onUp)
+	tableRef.value.removeEventListener('pointercancel',      onUp)
+	tableRef.value.removeEventListener('lostpointercapture', onUp)
+	tableRef.value.releasePointerCapture(e.pointerId)//should be the same as drag.pointer
+
+	drag = null//discard the drag object
 }
 
 </script>
