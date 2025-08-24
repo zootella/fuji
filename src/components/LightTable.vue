0575beb9040bf4d8f95311c5caf50fe19622bc91
Kevin Faaborg <zootella@zootella.com>
2025-08-24 17:46:49 -0600

    another note

--- src/components/LightTable.vue
+++ src/components/LightTable.vue
@@ -245,6 +245,7 @@ async function flip(direction) {
 	return flipQueue
 }
 //ttd august, you're seeing 30mb animated GIFs (that should be MPEGs) take 8s to get off disk in local development on windows, and with those in the list, you've been able to mangle the triad! new design: discard, rather than queuing, a second flip command while one is delayed, and immediately show a "Loading..." hud
+//two things that cause Loading... to appear. the current flip request is delayed for more than 200ms, the user is starting to wonder. or, there is a second flip request that we're tossing out while the current flip request is delayed
 async function _flip(direction) {
 	if (!folder) return//nothing loaded yet
 
